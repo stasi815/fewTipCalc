c73a2d98deb349104346544ec0f4950171f3682d
stasi815 <anastasia.gallardo@students.makeschool.com>
2020-02-20 14:24:06 -0800

    Calculates tip and splits bill

--- main.js
+++ main.js
@@ -7,46 +7,62 @@ const displayTotal = document.getElementById('displayTotal')
 const displayTip = document.getElementById('displayTip')
 const displaySplit = document.getElementById('displaySplit')
 
-const tipPercent = parseInt(inputTip.value)
-const bill = parseFloat(inputBill.value)
-const people = parseInt(inputPpl.value)
+// const tipPercent = parseInt(inputTip.value)
+// const bill = parseFloat(inputBill.value)
+// const people = parseInt(inputPpl.value)
 
-
-// Event handlers
-
-inputBill.addEventListener('change', function (e) {
-    handleBill()
-})
-
-inputTip.addEventListener('change', calculateTip())
-inputPpl.addEventListener('change', handlePpl)
 
 // Functions
 let bill = 0
 
 function calculateTotal() {
-    const bill = parseFloat
+    let bill = parseFloat(inputBill.value)
+    let tipPercent = parseInt(inputTip.value)
+    let people = parseInt(inputPpl.value)
+
+    let tip = bill * tipPercent/100
+    let total = bill + tip
+    let partySplit = total/people
+
+    displayTip.textContent = `\$ ${tip.toFixed(2)}`
+    displayTotal.textContent = `\$ ${total.toFixed(2)}`
+    displaySplit.textContent = `\$ ${partySplit.toFixed(2)}`
+
 }
 
-function handleBill(e) {
-    const newBill = parseFloat(e.target.value)
+// Event handlers
 
-    if (!isNaN(newBill)) {
-        bill = newBill
-    } else {
-        e.target.value = bill
-    }
-}
+inputBill.addEventListener('change', function (e) {
+    calculateTotal()
+})
 
-function calculateTip() {
-    const tip = bill * tipPercent/100
-    const total = bill + tip
-    const partySplit = total/people
+inputTip.addEventListener('change', function (e) {
+    calculateTotal()
+})
 
-    displayTip.innerHTML = tip.toFixed(2)
-    displayTotal.innerHTML = total.toFixed(2)
-    displayEach.innerHTML = partySplit.toFixed(2)
-}
+inputPpl.addEventListener('change', function (e) {
+    calculateTotal()
+})
+
+// function handleBill(e) {
+//     const newBill = parseFloat(e.target.value)
+
+//     if (!isNaN(newBill)) {
+//         bill = newBill
+//     } else {
+//         e.target.value = bill
+//     }
+// }
+
+// function calculateTip() {
+//     const tip = bill * tipPercent/100
+//     const total = bill + tip
+//     const partySplit = total/people
+
+//     displayTip.innerHTML = tip.toFixed(2)
+//     displayTotal.innerHTML = total.toFixed(2)
+//     displayEach.innerHTML = partySplit.toFixed(2)
+// }
 // function handleTip(e) {
 
 // }
